9715045bd5e779e88fec55cb6aa92811984dc66e
Safowaa <safowaabenedicta@gmail.com>
2024-05-11 21:57:45 +0000

    connect to the FrankFurter API, convert to DataFrame

--- Exchange_Rate_week6.py
+++ Exchange_Rate_week6.py
@@ -7,4 +7,22 @@ import seaborn as sns
 import numpy as np 
 
 
+# Define the start date and end date of the desired date range according to the question asked
+"2013-01-01..2023-12-31"
+# Construct the URL with the start date and end date
+url = "https://api.frankfurter.app/2013-01-01..2023-12-31"
+
+# Make a GET request to the API
+response = requests.get(url)
+
+# Turn it into a json
+data = response.json()
+data
+
+
+# Changing to a dataframe
+frank = pd.DataFrame(data)
+frank.head()
+
+
 
